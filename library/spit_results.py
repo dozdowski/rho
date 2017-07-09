91c0ef890a448319fcdba4d42e23f9d4b1d8129e
Noah Lavine <nlavine@redhat.com>
2017-07-09 02:30:17 -0700

    Initial integration of JBoss Scanner Linux scan commands
    Change the way Rho finds data to output to make it easy to incorporate
    new roles. Then import JBoss Scanner's roles for scanning Linux
    machines for JBoss.

--- library/spit_results.py
+++ library/spit_results.py
@@ -29,26 +29,84 @@ class Results(object):
         self.module = module
         self.name = module.params['name']
         self.file_path = module.params['file_path']
-        self.vals = module.params['vals']
+        self.all_vars = module.params['all_vars']
+        self.desired_facts = module.params['desired_facts']
 
     def write_to_csv(self):
         """Output report data to file in csv format"""
-        f_path = os.path.normpath(self.file_path)
-        with open(f_path, 'w') as write_file:
-            file_size = os.path.getsize(f_path)
-            vals = self.vals
-            fields = vals[0].keys()
-            fields.sort()
-            writer = csv.writer(write_file, delimiter=',')
+
+        # The results come in two parts. Results from the single
+        # Ansible module which includes all of Rho's Python modules
+        # appear in all_vars['res']. Other results appear as other
+        # keys in all_vars. To avoid printing extra Ansible
+        # information, we only output data that is known to come from
+        # Rho unless the user requests it via 'desired_facts':
+
+        # Keys that appear in 'res' objects
+        res_keys = set()
+        # Keys that appear in the top-level host vars
+        other_keys = set()
+
+        # Flatten the data and update res_keys and other_keys
+        flat_vars = {}
+        for host, data in self.all_vars.iteritems():
+            # Need to update the keys with every host's set in
+            # case some commands failed on some hosts but not
+            # others.
+            res_keys.update(data['res'].keys())
+
+            # flat_data is a dictionary in which all of the facts
+            # we want to output are keys, and their values are the
+            # corresponding values in the dict.
+            flat_data = data.copy()
+            del flat_data['res']
+            other_keys.update(flat_data.keys())
+
+            flat_data.update(data['res'])
+            flat_vars[host] = flat_data
+
+        # We know a key came from Rho if either it was in a 'res'
+        # attribute or it starts with 'rho'. If we don't know it's
+        # from Rho, we default to not printing it, because some
+        # Ansible keys contain information users probably wouldn't
+        # want to release accidentally (like the path to their SSH
+        # key). They can always override this by setting
+        # 'desired_facts'.
+        if self.desired_facts != ['default']:
+            keys = set(self.desired_facts)
+        else:
+            keys = res_keys
+            for key in list(other_keys):
+                if key.startswith('rho'):
+                    keys.add(key)
+
+        # Filter the data to only what we want. This is not
+        # necessary just for writing the CSV, but we also output
+        # this information as an Ansible variable.
+        filtered_vars = {host:
+                         {fact: value
+                          for (fact, value) in flat_data.iteritems()
+                          if fact in keys}
+                         for host, flat_data in flat_vars.iteritems()}
+
+        normalized_path = os.path.normpath(self.file_path)
+        with open(normalized_path, 'w') as write_file:
+            # Construct the CSV writer
+            writer = csv.DictWriter(write_file, sorted(keys), 
+                                    extrasaction='ignore', delimiter=',')
+
+            # Write a CSV header if necessary
+            file_size = os.path.getsize(normalized_path)
             if file_size == 0:
-                writer.writerow(fields)
-            for data in vals:
-                sorted_keys = data.keys()
-                sorted_keys.sort()
-                sorted_values = []
-                for k in sorted_keys:
-                    sorted_values.append(data[k])
-                writer.writerow(sorted_values)
+                writer.writeheader()
+
+            # Write the data
+            for data in filtered_vars.itervalues():
+                writer.writerow(data)
+
+        # Save the data for our results
+        self.filtered_vars = filtered_vars
+        self.keys = list(keys)
 
 
 def main():
@@ -59,15 +117,16 @@ def main():
     fields = {
         "name": {"required": True, "type": "str"},
         "file_path": {"required": True, "type": "str"},
-        "vals": {"required": True, "type": "list"}
+        "all_vars": {"required": True, "type": "dict"},
+        "desired_facts": {"required": True, "type": "list"}
     }
 
     module = AnsibleModule(argument_spec=fields)
 
     results = Results(module=module)
     results.write_to_csv()
-    vals = json.dumps(results.vals)
-    module.exit_json(changed=False, meta=vals)
+    module.exit_json(changed=False, meta={'filtered_vars': results.filtered_vars,
+                                          'keys': results.keys})
 
 
 if __name__ == '__main__':
